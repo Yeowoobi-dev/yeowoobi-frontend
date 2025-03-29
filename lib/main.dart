3a354dc14f27751354c0f3b70157dd07074f8f73
kwondoha <ehehgk@icloud.com>
2025-03-29 17:31:08 +0900

    feat: 컬러 시스템 적용

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,21 @@ import 'package:yeowoobi_frontend/widgets/custom_theme.dart';
 import 'package:yeowoobi_frontend/etc/screens/splash_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Yeowoobi',
-      theme: CustomTheme.lightTheme, // 라이트 테마 적용
-      darkTheme: CustomTheme.darkTheme, // 다크 테마 적용
-      themeMode: ThemeMode.system, // 시스템 설정
-      home: SplashScreen(),
+      theme: CustomTheme.lightTheme, // 라이트 테마
+      darkTheme: CustomTheme.darkTheme, // 다크 테마
+      themeMode: ThemeMode.system, // 시스템 설정 따라
+      home: SplashScreen(), // 시작 화면
     );
   }
 }
