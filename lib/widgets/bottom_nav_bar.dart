3958548b52983a167c9cad061cb74fea963df724
kwondoha <ehehgk@icloud.com>
2025-03-30 00:30:20 +0900

    feat: 하단바 아이콘 변경

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -5,45 +5,43 @@ class BottomNavBar extends StatelessWidget {
   final int currentIndex;
   final Function(int) onTap;
 
-  const BottomNavBar(
-      {required this.currentIndex, required this.onTap, super.key});
+  const BottomNavBar({
+    required this.currentIndex,
+    required this.onTap,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context); // ✅ 현재 테마 가져오기
-    final bool isDarkMode = theme.brightness == Brightness.dark; // ✅ 다크 모드 확인
+    final colorScheme = Theme.of(context).colorScheme;
 
     return BottomNavigationBar(
       currentIndex: currentIndex,
       onTap: onTap,
-      backgroundColor:
-          isDarkMode ? Colors.black : Colors.white, // ✅ 다크/라이트 모드별 배경색
-      selectedItemColor: isDarkMode
-          ? CustomTheme.darkPrimaryColor
-          : CustomTheme.lightPrimaryColor, // ✅ 선택된 아이콘 & 라벨 색상
-      unselectedItemColor: isDarkMode
-          ? CustomTheme.darkSecondaryColor
-          : CustomTheme.lightSecondaryColor, // ✅ 선택되지 않은 아이콘 & 라벨 색상
-      selectedLabelStyle: TextStyle(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      selectedItemColor: colorScheme.primary,
+      unselectedItemColor: CustomTheme.neutral200,
+      selectedLabelStyle: const TextStyle(
         fontSize: 12,
-        fontWeight: FontWeight.bold, // ✅ 선택된 라벨 가독성 강화
-        color: isDarkMode
-            ? CustomTheme.darkPrimaryColor
-            : CustomTheme.lightPrimaryColor,
+        fontWeight: FontWeight.bold,
       ),
-      unselectedLabelStyle: TextStyle(
+      unselectedLabelStyle: const TextStyle(
         fontSize: 12,
-        fontWeight: FontWeight.w500, // ✅ 선택되지 않은 라벨 가독성 강화
-        color: isDarkMode
-            ? CustomTheme.darkSecondaryColor
-            : CustomTheme.lightSecondaryColor,
+        fontWeight: FontWeight.w500,
       ),
-      type: BottomNavigationBarType.fixed, // ✅ 아이콘 움직임 없이 고정
       items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.book), label: "독서록"),
-        BottomNavigationBarItem(icon: Icon(Icons.people), label: "SNS"),
-        BottomNavigationBarItem(icon: Icon(Icons.forum), label: "게시판"),
-        BottomNavigationBarItem(icon: Icon(Icons.settings), label: "설정"),
+        BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('assets/icons/category.png')),
+            label: "독서록"),
+        BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('assets/icons/sns.png')), label: "SNS"),
+        BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('assets/icons/board.png')),
+            label: "게시판"),
+        BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('assets/icons/setting.png')),
+            label: "설정"),
       ],
     );
   }
